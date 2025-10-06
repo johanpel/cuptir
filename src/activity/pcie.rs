afaf8cdc733cd7409b891ba6f548514a9114bbd0
Johan Peltenburg <johan.peltenburg+code@gmail.com>
2025-10-06 15:45:32 +0200

    Add union fields

--- src/activity/pcie.rs
+++ src/activity/pcie.rs
@@ -1,8 +1,9 @@
-use cudarc::cupti::sys;
+use cudarc::{cupti::sys, driver};
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
-use crate::CuptirError;
+use crate::{CuptirError, utils::uuid_from_i8_slice};
 
 pub type DeviceType = crate::enums::PcieDeviceType;
 
@@ -31,10 +32,10 @@ impl From<&sys::CUpti_ActivityPcie> for Node {
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
 #[cfg_attr(feature = "serde", derive(Deserialize, Serialize))]
 pub struct GpuRecord {
-    pub device_id: cudarc::driver::sys::CUdevice,
+    pub device_id: driver::sys::CUdevice,
     pub node_props: Node,
-    // TODO:
-    // pub attr: CUpti_ActivityPcie__bindgen_ty_2,
+    pub device_uuid: Uuid,
+    pub peer_devices: [driver::sys::CUdevice; 32],
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
@@ -42,8 +43,9 @@ pub struct GpuRecord {
 pub struct BridgeRecord {
     pub bridge_id: u32,
     pub node_props: Node,
-    // TODO:
-    // pub attr: CUpti_ActivityPcie__bindgen_ty_2,
+    pub secondary_bus: u16,
+    pub device_id: u16,
+    pub vendor_id: u16,
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
@@ -58,13 +60,24 @@ impl TryFrom<&sys::CUpti_ActivityPcie> for Record {
 
     fn try_from(value: &sys::CUpti_ActivityPcie) -> Result<Self, Self::Error> {
         Ok(match value.type_.try_into()? {
-            DeviceType::Gpu => Self::Gpu(GpuRecord {
-                device_id: value.id.bindgen_union_field as i32,
-                node_props: value.into(),
+            DeviceType::Gpu => Self::Gpu({
+                let gpu_attr = unsafe { value.attr.gpuAttr.as_ref() };
+                GpuRecord {
+                    device_id: unsafe { value.id.devId.as_ref() }.clone() as driver::sys::CUdevice,
+                    node_props: value.into(),
+                    device_uuid: uuid_from_i8_slice(&gpu_attr.uuidDev.bytes),
+                    peer_devices: gpu_attr.peerDev.clone(),
+                }
             }),
-            DeviceType::Bridge => Self::Bridge(BridgeRecord {
-                bridge_id: value.id.bindgen_union_field,
-                node_props: value.into(),
+            DeviceType::Bridge => Self::Bridge({
+                let bridge_attr = unsafe { value.attr.bridgeAttr.as_ref() };
+                BridgeRecord {
+                    bridge_id: unsafe { value.id.bridgeId.as_ref() }.clone(),
+                    node_props: value.into(),
+                    secondary_bus: bridge_attr.secondaryBus,
+                    device_id: bridge_attr.deviceId,
+                    vendor_id: bridge_attr.vendorId,
+                }
             }),
         })
     }
